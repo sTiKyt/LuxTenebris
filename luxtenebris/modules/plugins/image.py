454aadf12889a579a7a538739e829b0279d9a15d
sTiKyt <stikyt@protonmail.com>
2022-01-15 15:13:23 +0200

    Added "as_file" option for image module

--- luxtenebris/modules/plugins/image.py
+++ luxtenebris/modules/plugins/image.py
@@ -15,10 +15,18 @@ async def command_image(client, message):
         await client.download_media(message.reply_to_message, file_name=media)
         img = Image.open(media)
         print(img.format) # DEBUG
-        await message.edit(f"This is **{img.format}** image, **converting to .png**...")
-        rgba = img.convert("RGBA")
-        rgba.save(f'{media}.png', 'png')
-        await client.send_document(chat_id=message.chat.id, document=f'{media}.png', reply_to_message_id=message.message_id)
+                    await message.edit(f"This is **{img.format}** image, **converting to .{extension}**...")
+                    if "as_file" in message_text.lower():
+                        if 'jpeg' in extension:
+                            img = img.convert("RGB")
+                        else:
+                            img = img.convert("RGBA")
+                        img.save(f'{media}.{extension}', extension)
+                        await client.send_document(chat_id=message.chat.id, document=f'{media}.{extension}', reply_to_message_id=message.message_id)
+                    else:
+                        img = img.convert("RGB")
+                        img.save(f'{media}.{extension}', extension)
+                        await client.send_photo(chat_id=message.chat.id, photo=f'{media}.{extension}', reply_to_message_id=message.message_id)
     except:
         await message.edit("This is **NOT** an **image**!")
         print('Not an image!') # DEBUG 
