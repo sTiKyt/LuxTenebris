0f893edd422e7acb0d356924d580ba3aef68251a
sTiKyt <stikyt@protonmail.com>
2022-01-15 10:02:01 +0200

    Rewrote help plugin to get rid of duplicated edits

--- luxtenebris/modules/plugins/help.py
+++ luxtenebris/modules/plugins/help.py
@@ -5,12 +5,12 @@ plugins_and_help['help_no_module'] = f"No valid **module** was specified, please
 
 @Client.on_message(filters.command('help', command_prefix))
 async def command_help(client, message):
+    not_edited = True
+    print(not_edited) # DEBUG
     for key in plugins_and_help:
-        if key in message.text.lower():
+        if key in message.text.lower() and not_edited == True:
             print(f'Showing help for {key}...') # DEBUG LOG STUFF
+            not_edited = False
             await message.edit(plugins_and_help[key])
-        else:
-            try:
-                await message.edit(plugins_and_help['help_no_module'])
-            except:
-                print("Message was already edited, skipping") #DEBUG LOG
+    if not_edited is True:
+        await message.edit(plugins_and_help['help_no_module'])
