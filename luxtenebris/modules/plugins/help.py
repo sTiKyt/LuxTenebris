91b8ab985d57b3833de4ccf395af95badbed429c
sTiKyt <stikyt@protonmail.com>
2022-01-15 09:05:56 +0200

    Wrote simple help plugin

---
+++ luxtenebris/modules/plugins/help.py
@@ -0,0 +1,17 @@
+from pyrogram import Client, filters
+from modules.clients.main_user import command_prefix
+from ..registry import plugins_and_help
+
+plugins_and_help['help_no_module'] = f"No valid **module** was specified, please provide **module** name: \n`{command_prefix}help [module]`"
+
+@Client.on_message(filters.command('help', command_prefix))
+async def command_help(client, message):
+    for key in plugins_and_help:
+        if key in message.text.lower():
+            print(f'Showing help for {key}...') # DEBUG LOG STUFF
+            await message.edit(plugins_and_help[key])
+        else:
+            try:
+                await message.edit(plugins_and_help['help_no_module'])
+            except:
+                print("Message was already edited, skipping") #DEBUG LOG
