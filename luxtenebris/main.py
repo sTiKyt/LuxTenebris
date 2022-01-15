112a7f3d519be10c2f9fdb895161ff9fab035685
sTiKyt <stikyt@protonmail.com>
2022-01-15 14:00:18 +0200

    Replaced shitty way to obtain current file directory with better implementation

--- luxtenebris/main.py
+++ luxtenebris/main.py
@@ -3,13 +3,13 @@ from pyrogram import Client
 
 
 def login():
-    if path.exists("./config.ini"):
+    if path.exists(f'{path.dirname(__file__)}/config.ini'):
         print('Credentials config found')
     else:
         print("Login at https://my.telegram.org/apps and")
         api_id = int(input("enter your api_id: "))
         api_hash = input("enter your api_hash: ")
-        with open(f'{str(__file__).replace("main.py", "")}/config.ini', 'w') as config: # I know this line looks like shit, fuck off
+        with open(f'{path.dirname(__file__)}/config.ini', 'w') as config:
             config.write(f"[pyrogram] \n api_id = {api_id} \n api_hash = {api_hash}")
 
 if __name__ == "__main__":
