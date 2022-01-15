63a5db5669e896d2492ae141d21bf7d156ca28fb
sTiKyt <stikyt@protonmail.com>
2022-01-15 10:10:38 +0200

    trying to get image command working

--- luxtenebris/modules/plugins/image.py
+++ luxtenebris/modules/plugins/image.py
@@ -3,6 +3,7 @@ from ..registry import plugins_and_help, command_prefix
 
 plugins_and_help['image'] = '**PLACEHOLDER**'
 
-@Client.on_message(filters.command('image', command_prefix))
+@Client.on_message()
 async def command_image(client, message):
-    await message.reply('test')
+    print(message)
+    await message.edit("123")
